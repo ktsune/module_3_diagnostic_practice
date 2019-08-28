38705806fef6903021b77a577f65242ade4a30a9
Sarah Tatro <statrox4@gmail.com>
2019-08-27 19:13:14 -0600

    JSON::ParserError: unexpected token at <!DOCTYPE html>

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,14 +1,17 @@
 class SearchFacade
   def nearest_stations
     # => make faraday object
-    conn = Faraday.new(:url => 'https://developer.nrel.gov/api/alt-fuel-stations/v1') do |f|
+    conn = Faraday.new(:url => 'https://developer.nrel.gov') do |f|
       f.headers['X-API-KEY'] = ENV['NREL_KEY']
       f.adapter  Faraday.default_adapter
     end
     # => make get request & parse json response
+    res = conn.get('/api/alt-fuel-stations/v1/nearest.json?fuel_type=ELEC,LPG&limit=10&location=Denver,CO')
+
     binding.pry
-    res = JSON.parse(conn.get('/nearest.json?fuel_type=ELEC,LPG&limit=10&location=Denver, CO'), symbolize_names: true)
+    JSON.parse(res.body, symbolize_names: true)
     # => turn json into objects
+
     # => move api call logic into a service if there is time
   end
 end
