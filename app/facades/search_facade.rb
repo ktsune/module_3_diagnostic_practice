3ba5553dcf73b42bb40d5a7908a9f527d072a879
Sarah Tatro <statrox4@gmail.com>
2019-08-27 18:54:35 -0600

    Set up faraday connection and start handling response

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,12 +1,14 @@
 class SearchFacade
   def nearest_stations
     # => make faraday object
-    conn = Faraday.new(:url => 'https://developer.nrel.gov/api/alt-fuel-stations/v1/')
-
+    conn = Faraday.new(:url => 'https://developer.nrel.gov/api/alt-fuel-stations/v1') do |f|
+      f.headers['X-API-KEY'] = ENV['NREL_KEY']
+      f.adapter  Faraday.default_adapter
+    end
     # => make get request & parse json response
-
+    binding.pry
+    res = JSON.parse(conn.get('/nearest.json?fuel_type=ELEC,LPG&limit=10&location=Denver, CO'), symbolize_names: true)
     # => turn json into objects
-
     # => move api call logic into a service if there is time
   end
 end
