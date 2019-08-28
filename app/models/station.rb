8d454be590fe343dfe32b752bc3d7c2bf9a6009a
Sarah Tatro <statrox4@gmail.com>
2019-08-27 19:56:34 -0600

    Format response

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,10 +2,10 @@ class Station
   attr_reader :name, :address, :fuel_type, :distance, :access_times
 
   def initialize(data)
-    @name = data[:name]
-    @address = data[:address]
-    @fuel_type = data[:fuel_type]
+    @name = data[:station_name]
+    @address = data[:street_address]
+    @fuel_type = data[:fuel_type_code]
     @distance = data[:distance]
-    @access_times = data[:access_times]
+    @access_times = data[:access_days_time]
   end
 end
